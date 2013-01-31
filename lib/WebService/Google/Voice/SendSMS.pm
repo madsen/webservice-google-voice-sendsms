293f41ffb481fbb8080109f3777f24318a49dc60
Christopher J. Madsen <perl@cjmweb.net>
2013-01-30 18:29:39 -0600

    Initial working version

--- lib/WebService/Google/Voice/SendSMS.pm
+++ lib/WebService/Google/Voice/SendSMS.pm
@@ -14,17 +14,105 @@ package WebService::Google::Voice::SendSMS;
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See either the
 # GNU General Public License or the Artistic License for more details.
 #
-# ABSTRACT:
+# ABSTRACT: Send a SMS using Google Voice
 #---------------------------------------------------------------------
 
 use 5.008;
 use strict;
 use warnings;
 
+use LWP::UserAgent 6 ();
+use HTTP::Request::Common ();
+
 our $VERSION = '0.01';
 # This file is part of {{$dist}} {{$dist_version}} ({{$date}})
 
 #=====================================================================
+
+sub inboxURL { 'https://www.google.com/voice/m/' }
+sub loginURL { 'https://www.google.com/accounts/ClientLogin' }
+sub smsURL   { 'https://www.google.com/voice/m/sendsms' }
+
+sub new
+{
+  my ($class, $username, $password) = @_;
+
+  bless {
+    username => $username,
+    password => $password,
+    ua       => LWP::UserAgent->new(
+      agent => "Mozilla/5.0 (iPhone; U; CPU iPhone OS 2_2_1 like Mac OS X;".
+               " en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) ".
+               "Version/3.1.1 Mobile/5H11 Safari/525.20",
+      cookie_jar => {},
+    ),
+  }, $class;
+} # end new
+
+sub _login
+{
+  my $self = shift;
+
+  my $req = HTTP::Request::Common::POST($self->loginURL, [
+    accountType => 'GOOGLE',
+    Email       => $self->{username},
+    Passwd      => $self->{password},
+    service     => 'grandcentral',
+    source      => 'org.cpan.WebService.Google.Voice.SendSMS',
+  ]);
+
+  $req->referer($self->{lastURL});
+
+  my $rsp = $self->{ua}->request($req);
+  die $rsp->status_line unless $rsp->is_success;
+  $self->{lastURL} = $rsp->request->uri;
+
+  my $cref = $rsp->decoded_content(ref => 1);
+  $$cref =~ /Auth=([A-z0-9_-]+)/ or die "no auth: $$cref";
+
+  return $self->{login_auth} = $1;
+}
+
+sub _get_rnr_se
+{
+  my $self = shift;
+
+  my $login_auth = $self->_login;
+
+  my $req = HTTP::Request::Common::GET($self->inboxURL);
+
+  $req->header(Authorization => "GoogleLogin auth=$login_auth");
+  #$req->referer($self->{lastURL});
+
+  my $rsp = $self->{ua}->request($req);
+  die $rsp->status_line unless $rsp->is_success;
+  $self->{lastURL} = $rsp->request->uri;
+
+  my $cref = $rsp->decoded_content(ref => 1);
+  $$cref =~ /<input.*?name="_rnr_se".*?value="(.*?)"/s or die "no rnr : $$cref";
+
+  return $1;
+} # end _get_rnr_se
+
+sub send_sms
+{
+  my ($self, $number, $message) = @_;
+
+  my $req = HTTP::Request::Common::POST($self->smsURL, [
+    id      => '',
+    c       => '',
+    number  => $number,
+    smstext => $message,
+    _rnr_se => $self->_get_rnr_se,
+  ]);
+
+  $req->header(Authorization => "GoogleLogin auth=$self->{login_auth}");
+  $req->referer($self->{lastURL});
+
+  my $rsp = $self->{ua}->request($req);
+  $self->{lastURL} = $rsp->request->uri;
+  return $rsp->is_success;
+} # end send_sms
 
 #=====================================================================
 # Package Return Value:
@@ -36,3 +124,9 @@ __END__
 =head1 SYNOPSIS
 
   use WebService::Google::Voice::SendSMS;
+
+  my $sender = WebService::Google::Voice::SendSMS->new(
+    $email_address, $password
+  );
+
+  $sender->send_sms($phone_number => $message);
